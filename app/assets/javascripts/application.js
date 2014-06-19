44276fea63823cda1c8af6d9ad31924e891cd2af
rborkows <rborkows@gmail.com>
2014-06-18 22:45:59 -0700

    Fancy Upload Buttons!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
 //= require turbolinks
 //= require_tree .
 // Loads all Bootstrap javascripts
 //= require bootstrap
+//= require jquery-fileupload
